2408646aeb0edb8983e60dc1205e351b1e17a76b
Photokub <photokub@yandex.ru>
2023-03-01 23:44:34 +0300

    Fix: удаление фильма

--- routes/movies.js
+++ routes/movies.js
@@ -6,6 +6,6 @@ const { validateMovieCreation, validateMovieId } = require('../middlewares/valid
 
 router.get('/', getMovies);
 router.post('/', validateMovieCreation, saveMovie);
-router.delete('/:movieid ', validateMovieId, deleteMovie);
+router.delete('/:movieId ', validateMovieId, deleteMovie);
 
 module.exports = router;
