44d221ec6e17a38bff966403538432942ca1026c
Photokub <photokub@yandex.ru>
2023-03-01 23:24:02 +0300

    Fix: валидация validateMovieId

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -62,6 +62,6 @@ module.exports.validateMovieCreation = celebrate({
 
 module.exports.validateMovieId = celebrate({
   params: Joi.object().keys({
-    _id: Joi.string().required().hex().length(24),
+    movieId: Joi.string().required().hex().length(24),
   }),
 });
