6855dc30ec54b90bf654b3e54355064264d776c0
Photokub <photokub@yandex.ru>
2023-03-01 22:53:22 +0300

    Fix: movieSchema

--- routes/movies.js
+++ routes/movies.js
@@ -5,7 +5,7 @@ const { getMovies, saveMovie, deleteMovie } = require('../controllers/movies');
 const { validateMovieCreation, validateMovieId } = require('../middlewares/validators');
 
 router.get('/', getMovies);
-router.post('/movies', validateMovieCreation, saveMovie);
+router.post('/', validateMovieCreation, saveMovie);
 router.delete('/:_id ', validateMovieId, deleteMovie);
 
 module.exports = router;
