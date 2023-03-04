f1f89c8c946ef534811a92750a1d9b731862ae6d
Photokub <photokub@yandex.ru>
2023-03-04 22:22:21 +0300

    Fix: удаление карточки

--- controllers/movies.js
+++ controllers/movies.js
@@ -14,10 +14,18 @@ const getMovies = async (req, res, next) => {
   }
 };
 
-const getCurrentMovie = async (req, res, next) => {
+//TODO// const getCurrentMovie = async (req, res, next) => {
+//   try {
+//     const movie = await Movie.findById({ id: req.params._id }).populate('owner');
+//     return res.send(movie);
+//   } catch (err) {
+//     return next(err);
+//   }
+// };
+
+const paramsTest = async (req, res, next) => {
   try {
-    const movie = await Movie.findById({ id: req.params._id }).populate('owner');
-    return res.send(movie);
+    res.send(req.params);
   } catch (err) {
     return next(err);
   }
@@ -62,5 +70,6 @@ module.exports = {
   getMovies,
   saveMovie,
   deleteMovie,
-  getCurrentMovie
+  //getCurrentMovie,
+  paramsTest
 };
--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 
-const { getMovies, saveMovie, deleteMovie, getCurrentMovie } = require('../controllers/movies');
+const { getMovies, saveMovie, deleteMovie, paramsTest} = require('../controllers/movies');
 
 const { validateMovieCreation, validateMovieId } = require('../middlewares/validators');
 
 router.get('/', getMovies);
 router.post('/', validateMovieCreation, saveMovie);
 router.delete('/:_id ', deleteMovie);
-router.get('/:id ', getCurrentMovie);
+//router.get('/:id ', getCurrentMovie);
+router.get('/:id ', paramsTest);
 //router.delete('/:_id ', validateMovieId, deleteMovie);
 
 module.exports = router;
