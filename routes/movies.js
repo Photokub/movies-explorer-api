0ca01378345aef25e082b288345f5757ed688c1c
Photokub <photokub@yandex.ru>
2023-03-04 22:44:26 +0300

    Refactor: paramsTest

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { getMovies, saveMovie, deleteMovie, paramsTest} = require('../controllers/movies');
+const { getMovies, saveMovie, deleteMovie} = require('../controllers/movies');
 
 const { validateMovieCreation, validateMovieId } = require('../middlewares/validators');
 
@@ -8,7 +8,10 @@ router.get('/', getMovies);
 router.post('/', validateMovieCreation, saveMovie);
 router.delete('/:_id ', deleteMovie);
 //router.get('/:id ', getCurrentMovie);
-router.get('/:id ', paramsTest);
+//router.get('/:id ', paramsTest);
+app.get('/:id', (req, res) => {
+    res.send(req.params);  
+  });
 //router.delete('/:_id ', validateMovieId, deleteMovie);
 
 module.exports = router;
