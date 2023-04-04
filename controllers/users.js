476c03f6798fef4930d889c0e84e9e84c3cae1eb
A_Kub_work <photokub@yandex.ru>
2023-04-04 12:16:52 +0300

    Refactor: изменение параметра куки SameSite при регистрации

--- controllers/users.js
+++ controllers/users.js
@@ -45,7 +45,7 @@ const createUser = async (req, res, next) => {
         {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: 'Lax',
+          sameSite: 'Strict',
           secure: true,
         })
       .send({
